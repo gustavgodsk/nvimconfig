52a693d5e048bac7f67b3b452f6d6537ceb0ec00
gustavgodsk <gustavgodsk@gmail.com>
2026-03-18 11:32:19 +0100

    fixed svelte, html, css formatting

--- lua/plugins/02-lsp.lua
+++ lua/plugins/02-lsp.lua
@@ -1,207 +1,163 @@
 -- ~/.config/nvim/lua/plugins/02-lsp.lua
 
 return {
-  -- Treesitter
-  {
-    "nvim-treesitter/nvim-treesitter",
-    event = { "BufReadPre", "BufNewFile" }, -- Load immediately when opening a file
-    build = ":TSUpdate",
-    config = function()
-      vim.treesitter.language.register('xml', 'xaml')
+    -- Treesitter
+    {
+        "nvim-treesitter/nvim-treesitter",
+        event = { "BufReadPre", "BufNewFile" },
+        build = ":TSUpdate",
+        config = function()
+            vim.treesitter.language.register('xml', 'xaml')
 
-      -- run setup
-      require("nvim-treesitter.configs").setup({
-        ensure_installed = {
-          "c_sharp",
-          "lua",
-          "vim",
-          "java",
-          "typst",
-          "xml",
-          "cpp",
-          "javascript",
-          "typescript",
-          "markdown",
-          "markdown_inline",
-          "html"
-        },
-        highlight = { 
-          enable = true,
-          additional_vim_regex_highlighting = false 
-        },
-        indent = { enable = true },
-        auto_install = true,
-      })
-    end,
-  },
-
-  -- Core LSP configuration
-  { "neovim/nvim-lspconfig" },
-
-  -- Mason
-  {
-    "mason-org/mason.nvim",
-    enabled = _G.IS_WINDOWS,
-    config = function()
-      require("mason").setup({
-        ensure_installed = { 
-          "csharp_ls", 
-          "clangd",
-          "jdtls", 
-          "java-debug-adapter", 
-          "java-test", 
-          "tinymist",
-          "prettier",
-          "typescript-language-server",
-          "svelte-language-server",
-          "netcoredbg"
-        }
-      })
-    end,
-  },
-
-  -- Mason-LSPConfig
-  {
-    "mason-org/mason-lspconfig.nvim",
-    dependencies = { "mason-org/mason.nvim", "hrsh7th/nvim-cmp", "hrsh7th/cmp-nvim-lsp" },
-    enabled = _G.IS_WINDOWS,
-    config = function()
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
-      require("mason-lspconfig").setup({
-        ensure_installed = { "csharp_ls", "tinymist", "ts_ls" },
-
-        handlers = {
-          -- Default handler (applies to csharp_ls, ts_ls, etc.)
-          function(server_name)
-            require("lspconfig")[server_name].setup({
-              capabilities = capabilities,
+            require("nvim-treesitter.configs").setup({
+                ensure_installed = {
+                    "c_sharp", "lua", "vim", "java", "typst", "xml", "cpp",
+                    "javascript", "typescript", "markdown", "markdown_inline",
+                    "html", "css", "tsx", -- HTML and CSS are already here!
+                },
+                highlight = { 
+                    enable = true,
+                    additional_vim_regex_highlighting = false 
+                },
+                indent = { enable = true },
+                auto_install = true,
             })
-          end,
+        end,
+    },
 
-          ["jdtls"] = function() end, -- Handled by lang-java.lua
+    -- Core LSP configuration
+    { "neovim/nvim-lspconfig" },
 
-          -- Tinymist (Typst)
-          ["tinymist"] = function()
-            require("lspconfig")["tinymist"].setup({
-              capabilities = capabilities,
-              settings = {
-                formatterMode = "typstyle",
-                exportPdf = "onSave", 
-                semanticTokens = "disable",
-              }
+    -- Mason
+    {
+        "mason-org/mason.nvim",
+        enabled = _G.IS_WINDOWS,
+        config = function()
+            require("mason").setup({
+                ensure_installed = { 
+                    "csharp_ls", "clangd", "jdtls", "java-debug-adapter", 
+                    "java-test", "tinymist", "prettier",
+                    "typescript-language-server", "svelte-language-server", "netcoredbg",
+                    -- NEW: Added HTML, CSS, and Emmet to Mason
+                    "html-lsp", "css-lsp", "emmet-ls" 
+                }
             })
-          end,
-        }
-      })
-    end,
-  },
+        end,
+    },
 
-  -- Autocompletion (nvim-cmp)
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = { "hrsh7th/cmp-nvim-lsp", "hrsh7th/cmp-buffer", "hrsh7th/cmp-path", "L3MON4D3/LuaSnip", "saadparwaiz1/cmp_luasnip" },
-    event = "InsertEnter",
-    config = function()
-      local cmp = require("cmp")
-      local luasnip = require("luasnip")
-      cmp.setup({
-        snippet = { expand = function(args) luasnip.lsp_expand(args.body) end },
-        sources = cmp.config.sources({
-          { name = "nvim_lsp" },
-    { name = "luasnip" },
-    { name = "buffer" },
-    { name = "path" },
-  }),
-  mapping = cmp.mapping.preset.insert({
-    ["<Tab>"] = cmp.mapping.select_next_item(),
-    ["<S-Tab>"] = cmp.mapping.select_prev_item(),
-    ["<CR>"] = cmp.mapping.confirm(),
-    -- ["<CR>"] = cmp.mapping.confirm({ select = true }),
-    ["<C-Space>"] = cmp.mapping.complete(),
-  }),
-  })
-  end,
-  },
-  -- 7. INDENT BLANKLINE 
-  -- { 
-  --     "lukas-reineke/indent-blankline.nvim", 
-  --     dependencies = { "nvim-treesitter/nvim-treesitter" }, 
-  --     event = { "BufReadPre", "BufNewFile" }, 
-  --     main = "ibl", 
-  --     opts = { 
-  --         -- 1. DISABLE STATIC LINES
-  --         indent = { 
-  --             char = " ",  -- Set the character to an empty string (invisible)
-  --         }, 
-  --
-  --         -- 2. KEEP ACTIVE SCOPE (Only shows when cursor is inside)
-  --         scope = { 
-  --             enabled = true, 
-  --             show_start = false, 
-  --             show_end = false, 
-  --             char = "│",       -- Use the bar character ONLY for the active scope
-  --             highlight = "IblScope", 
-  --             priority = 500, 
-  --             -- You can keep your exclusions here if you want, 
-  --             -- but they matter less now since static lines are gone.
-  --             exclude = {
-  --                 node_type = {
-  --                     ["*"] = {
-  --                         "if_statement",
-  --                         "for_statement",
-  --                         "while_statement",
-  --                         "switch_statement",
-  --                         "try_statement",
-  --                         "catch_clause",
-  --                     },
-  --                 },
-  --             },
-  --         }, 
-  --     }, 
-  --     config = function(_, opts) 
-  --         -- Optional: Make the scope line slightly brighter since it's the only one you'll see
-  --         vim.api.nvim_set_hl(0, "IblScope", { fg = "#263c42", bold = false }) 
-  --         require("ibl").setup(opts) 
-  --     end,
-  -- },
+    -- Mason-LSPConfig
+    {
+        "mason-org/mason-lspconfig.nvim",
+        dependencies = { "mason-org/mason.nvim", "hrsh7th/nvim-cmp", "hrsh7th/cmp-nvim-lsp" },
+        enabled = _G.IS_WINDOWS,
+        config = function()
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-  {
-    'stevearc/conform.nvim',
-    event = { "BufReadPre", "BufNewFile" },
-    config = function()
-    require("conform").setup({
-      formatters_by_ft = {
-        xml = { "xamlstyler" },
-        xaml = { "xamlstyler" },
-        javascript = { "prettier" },
-        typescript = { "prettier" },
-        html = { "prettier" },
-        json = { "prettier" },
-      },
-      formatters = {
-        prettier = {
-          prepend_args = { "--plugin", "@prettier/plugin-xml" },
-        },
+            require("mason-lspconfig").setup({
+                -- NEW: Added html, cssls, and emmet_ls to ensure_installed
+                ensure_installed = { "csharp_ls", "tinymist", "ts_ls", "html", "cssls", "emmet_ls" },
+
+                handlers = {
+                    -- Default handler (this will automatically catch and setup html and cssls correctly)
+                    function(server_name)
+                        require("lspconfig")[server_name].setup({
+                            capabilities = capabilities,
+
+                        })
+                    end,
+
+                    ["svelte"] = function()
+                        require("lspconfig")["svelte"].setup({
+                            capabilities = capabilities,
+                        })
+                    end,
+
+
+                    ["jdtls"] = function() end, -- Handled by lang-java.lua
+
+                    -- NEW: Specific handler for Emmet to define filetypes (including Svelte/TSX from your config)
+                    ["emmet_ls"] = function()
+                        require("lspconfig")["emmet_ls"].setup({
+                            capabilities = capabilities,
+                            filetypes = { "html", "css", "sass", "scss", "less", "javascriptreact", "typescriptreact", "svelte" },
+                        })
+                    end,
+
+                    -- Tinymist (Typst)
+                    ["tinymist"] = function()
+                        require("lspconfig")["tinymist"].setup({
+                            capabilities = capabilities,
+                            settings = {
+                                formatterMode = "typstyle",
+                                exportPdf = "onSave", 
+                                semanticTokens = "disable",
+                            }
+                        })
+                    end,
+                }
+            })
+        end,
+    },
+
+    -- Autocompletion (nvim-cmp)
+    {
+        "hrsh7th/nvim-cmp",
+        dependencies = { "hrsh7th/cmp-nvim-lsp", "hrsh7th/cmp-buffer", "hrsh7th/cmp-path", "L3MON4D3/LuaSnip", "saadparwaiz1/cmp_luasnip" },
+        event = "InsertEnter",
+        config = function()
+            local cmp = require("cmp")
+            local luasnip = require("luasnip")
+            cmp.setup({
+                snippet = { expand = function(args) luasnip.lsp_expand(args.body) end },
+                sources = cmp.config.sources({
+                    { name = "nvim_lsp" },
+                    { name = "luasnip" },
+                    { name = "buffer" },
+                    { name = "path" },
+                }),
+                mapping = cmp.mapping.preset.insert({
+                    ["<Tab>"] = cmp.mapping.select_next_item(),
+                    ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+                    ["<CR>"] = cmp.mapping.confirm(),
+                    ["<C-Space>"] = cmp.mapping.complete(),
+                }),
+            })
+        end,
+    },
+
+    -- Conform
+    {
+        'stevearc/conform.nvim',
+        event = { "BufReadPre", "BufNewFile" },
+        config = function()
+            require("conform").setup({
+                formatters_by_ft = {
+                    xml = { "xamlstyler" },
+                    xaml = { "xamlstyler" },
+                    javascript = { "prettier" },
+                    typescript = { "prettier" },
+                    html = { "prettier" },
+                    css = { "prettier" }, -- NEW: Mapped CSS to Prettier
+                    json = { "prettier" },
+                },
+                formatters = {
+                    prettier = {
+                        prepend_args = { "--plugin", "@prettier/plugin-xml" },
+                    },
                     xamlstyler = {
                         command = "xstyler",
-                        -- stdin = false is required because xstyler modifies files in-place
                         stdin = false, 
                         args = function(self, ctx)
-                            -- 1. Search for the config file starting from the current file's directory
-                            --    This works regardless of where you opened nvim from.
                             local config_path = vim.fs.find("Settings.XamlStyler", {
-                                path = ctx.dirname, -- Start search from the file's folder
-                                upward = true,      -- Search up the tree
-                                stop = vim.loop.os_homedir(), -- Stop at home dir
+                                path = ctx.dirname,
+                                upward = true,     
+                                stop = vim.loop.os_homedir(), 
                                 type = "file"
                             })[1]
 
-                            -- 2. If found, explicitly tell xstyler where it is using -c
                             if config_path then
                                 return { "-f", "$FILENAME", "-c", config_path }
                             else
-                                -- Fallback (unlikely to work well, but safe)
                                 return { "-f", "$FILENAME" }
                             end
                         end,
@@ -211,22 +167,17 @@ return {
                     timeout_ms = 3000,
                     lsp_fallback = true,
                 },
-    })
+            })
 
-    -- THE FIX: Strip BOM <feff> on save
-    -- This MUST be placed after conform.setup so it runs *after* formatting
-    vim.api.nvim_create_autocmd("BufWritePre", {
-      pattern = { "*.xaml", "*.xml" },
-      callback = function()
-        -- Save cursor position to avoid jumping
-        local cur = vim.fn.getpos(".")
-        -- Remove the BOM character (U+FEFF) from the first line only
-        -- 'silent!' prevents errors if the tag isn't there
-        vim.cmd("silent! 1s/^\\%uFEFF//e")
-        -- Restore cursor
-        vim.fn.setpos(".", cur)
-      end,
-    })
-  end,
-  }
+            -- THE FIX: Strip BOM <feff> on save
+            vim.api.nvim_create_autocmd("BufWritePre", {
+                pattern = { "*.xaml", "*.xml" },
+                callback = function()
+                    local cur = vim.fn.getpos(".")
+                    vim.cmd("silent! 1s/^\\%uFEFF//e")
+                    vim.fn.setpos(".", cur)
+                end,
+            })
+        end,
+    }
 }
--- lua/plugins/markview.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "OXY2DEV/markview.nvim",
-    lazy = false,
-
-    -- Completion for `blink.cmp`
-    -- dependencies = { "saghen/blink.cmp" },
-    vim.keymap.set('n', '<leader>nt', function() vim.cmd(":Markview Toggle") end, { desc = "Toggle Markview" }),
-    vim.keymap.set('n', '<leader>ns', function() vim.cmd(":Markview splitToggle") end, { desc = "Toggle Markview" }),
-};
