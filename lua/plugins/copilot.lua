45d8686ab3ff711ed0c286b987717a46a84eb824
gustavgodsk <gustavgodsk@gmail.com>
2026-04-10 20:31:04 +0200

    added keymap to turn off copilot suggestions

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -16,8 +16,18 @@ return {
                         dismiss = "<C-]>",
                     },
                 },
-                panel = { enabled = false }, -- We don't need this, Sidekick is better
+                panel = { enabled = false },
             })
+            vim.keymap.set("n", "<leader>ct", function()
+                require("copilot.suggestion").toggle_auto_trigger()
+
+                -- Optional: Add a little notification so you know if it's on or off
+                if vim.b.copilot_suggestion_auto_trigger then
+                    vim.notify("Copilot Auto-Suggestions: ON", vim.log.levels.INFO)
+                else
+                    vim.notify("Copilot Auto-Suggestions: OFF", vim.log.levels.WARN)
+                end
+            end, { desc = "Toggle Copilot Auto-Suggestions" })
         end,
     }
 }
